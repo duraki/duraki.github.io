33e46e2d89874e9a9d3194f03c49f8e1aab1e7a9
Halis Duraki <duraki.halis@nsoft.ba>
2017-07-07 21:58:34 +0200

    [added] scripts

---
+++ scripts/trigger_alina.c
@@ -0,0 +1,16 @@
+#include <iostream.h>
+
+// dn5 // http://dn5.ljuska.org
+// @dn5__
+
+int main()
+{
+    char traka1[100] = "%B4730770006751710^MARKO^14051015000000420000000?";
+    char traka2[100] = ";4730770006751710=140510110000420?\n";
+
+    printf(traka1); // Traka 1
+    printf("\n");
+    printf(traka2); // Traka 2
+    getchar(); // grrr
+    return 0;
+}
